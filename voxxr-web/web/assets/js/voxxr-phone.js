f2dbbe92567da3502952ae439f3a0c912330590e
Xavier Hanin <xavier.hanin@gmail.com>
2012-02-05 22:08:09 +0100

    implement remaining and since labels on main event page + auto refresh now playing. still need to call event.quit when going back to events page to avoid keeping unused crons

--- voxxr-web/web/assets/js/voxxr-phone.js
+++ voxxr-web/web/assets/js/voxxr-phone.js
@@ -206,30 +206,102 @@ $(function() {
         self.id = ko.observable(data.id);
         self.title = ko.observable(data.title);
         self.subtitle = ko.observable(data.subtitle);
+        self.from = ko.observable(data.from);
+        self.to = ko.observable(data.to);
         self.nbPresentations = ko.observable(data.nbPresentations);
         self.dates = ko.observable(data.dates);
         self.nowplaying = ko.observableArray([]);
         self.days = ko.observableArray(_(data.days).map(function(day) { return voxxr.scheduleDay(day);}));
+        self.remaining = ko.observable();
+        self.since = ko.observable();
+        var crons = {};
+
+        function updateRemaining() {
+            var remainingSeconds = remaining.getSeconds(self.from());
+            if (remainingSeconds > 0) {
+                self.remaining(remaining.getString(remainingSeconds) + " remaining");
+                crons.remaining = setTimeout(updateRemaining, 1000);
+            } else {
+                self.remaining("");
+                self.refreshNowPlaying();
+            }
+        }
+        function updateSince() {
+            var sinceSeconds = -remaining.getSeconds(self.to());
+            if (sinceSeconds > 0) {
+                self.since(remaining.getString(sinceSeconds) + " ago");
+                crons.since = setTimeout(updateSince, 1000);
+            } else {
+                self.since("");
+            }
+        }
 
         self.refreshNowPlaying = function() {
-            $.getJSON(baseUrl + "/events/" + self.id() + "/nowplaying", function(data) {
-                var wasplaying = self.nowplaying();
+            console.log("refreshing now playing");
+            $.ajax({
+                url: baseUrl + "/events/" + self.id() + "/nowplaying",
+                dataType:"json",
+                success: function(data) {
+                    var wasplaying = self.nowplaying();
 
-                self.nowplaying(_(data).map(function(presentation) {
-                    var p = voxxr.presentation(presentation);
-                    p.playing(true);
-                    p.room().currentPresentation(p);
-                    wasplaying = _(wasplaying).reject(function(e) { return e.id() === p.id() });
-                    return p;
-                }));
+                    self.nowplaying(_(data).map(function(presentation) {
+                        var p = voxxr.presentation(presentation);
+                        p.playing(true);
+                        p.room().currentPresentation(p);
+                        wasplaying = _(wasplaying).reject(function(e) { return e.id() === p.id() });
+                        return p;
+                    }));
 
-                // reset playing status for old nowplaying presentations
-                _(wasplaying).each(function(p) { 
-                    p.playing(false);
-                    if (p.room().currentPresentation() === p) {
-                        p.room().currentPresentation(null);
+                    // reset playing status for old nowplaying presentations
+                    _(wasplaying).each(function(p) {
+                        p.playing(false);
+                        if (p.room().currentPresentation() === p) {
+                            p.room().currentPresentation(null);
+                        }
+                    });
+
+                    // auto update
+                    if (crons.refreshNowPlaying) {
+                        clearTimeout(crons.refreshNowPlaying);
+                        crons.refreshNowPlaying = null;
                     }
-                });
+                    var refreshIn = 15000;
+                    if (data.length === 0) {
+                        var remainingSeconds = remaining.getSeconds(self.from());
+                        if (remainingSeconds > 0) {
+                            console.log("event not started yet");
+                            refreshIn = (remainingSeconds * 1000) - (5 * 60 * 1000);
+                        } else {
+                            var sinceSeconds = -remaining.getSeconds(self.to());
+                            if (sinceSeconds > (5 * 60 * 1000)) {
+                                console.log("event finished");
+                                return;
+                            }
+                        }
+                    }
+                    refreshIn = Math.max(refreshIn, 15000);
+                    console.log("scheduling now playing auto refresh in ", (refreshIn / 1000), " s");
+                    crons.refreshNowPlaying = setTimeout(self.refreshNowPlaying, refreshIn);
+                },
+                error: function() {
+                    console.log("error when getting now playing, retry in 15 seconds");
+                    crons.refreshNowPlaying = setTimeout(self.refreshNowPlaying, 15000);
+                }
+            });
+        }
+
+        self.enter = function() {
+            self.refreshNowPlaying();
+            updateRemaining();
+            updateSince();
+        }
+
+        self.quit = function() {
+            _(crons).each(function(cron, key) {
+                if (cron) {
+                    clearTimeout(cron);
+                    crons[key] = null;
+                }
             });
         }
 
@@ -271,7 +343,7 @@ $(function() {
 
         // change current selection function
         self.selectEvent = function(event) {
-            event.refreshNowPlaying();
+            event.enter();
             self.chosenEvent(event);
         };
 
