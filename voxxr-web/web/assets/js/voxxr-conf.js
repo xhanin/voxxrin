83821c6324a37826bf2bac22c7a3e1ebce6dd86d
Xavier Hanin <xavier.hanin@gmail.com>
2012-03-12 21:48:13 +0100

    change base url based on url params

--- voxxr-web/web/assets/js/voxxr-conf.js
+++ voxxr-web/web/assets/js/voxxr-conf.js
@@ -2,5 +2,8 @@
     exports.models = exports.models || {};
 //    exports.models.baseUrl = "http://localhost:8080/r";
 //    exports.models.baseUrl = "http://3.latest.voxxr-web.appspot.com/r";
-    exports.models.baseUrl = "http://voxxr-web.appspot.com/r";
+    exports.models.baseUrl = "http://app.voxxr.in/r";
+    if (urlParams['mode'] === 'dev') {
+        exports.models.baseUrl = "http://localhost:8080/r";
+    }
 })(window)
