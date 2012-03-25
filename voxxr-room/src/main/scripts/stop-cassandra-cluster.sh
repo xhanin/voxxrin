63ff7ee08cd8fdf6502e5abd7402305a6060e39b
Xavier Hanin <xavier.hanin@gmail.com>
2012-03-25 14:58:37 +0200

    add all possible nodes to stop cassandra cluster script

--- voxxr-room/src/main/scripts/stop-cassandra-cluster.sh
+++ voxxr-room/src/main/scripts/stop-cassandra-cluster.sh
@@ -3,3 +3,6 @@
 ovhcloud instance stopInstance --instanceId 69191
 ovhcloud instance stopInstance --instanceId 69253
 ovhcloud instance stopInstance --instanceId 69254
+ovhcloud instance stopInstance --instanceId 69446
+ovhcloud instance stopInstance --instanceId 69447
+ovhcloud instance stopInstance --instanceId 69448
