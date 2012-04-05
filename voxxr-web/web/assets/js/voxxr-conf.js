3a8ee3defadc1bccf2662edaacf7c6ecc05ab7e7
Xavier Hanin <xavier.hanin@gmail.com>
2012-04-06 01:41:40 +0200

    latest appengine is now 4

--- voxxr-web/web/assets/js/voxxr-conf.js
+++ voxxr-web/web/assets/js/voxxr-conf.js
@@ -1,7 +1,7 @@
 (function(exports) {
     exports.models = exports.models || {};
 //    exports.models.baseUrl = "http://localhost:8080/r";
-//    exports.models.baseUrl = "http://3.latest.voxxr-web.appspot.com/r";
+//    exports.models.baseUrl = "http://4.latest.voxxr-web.appspot.com/r";
     exports.models.baseUrl = "http://app.voxxr.in/r";
     if (urlParams['mode'] === 'dev') {
         exports.models.baseUrl = "http://localhost:8080/r";
