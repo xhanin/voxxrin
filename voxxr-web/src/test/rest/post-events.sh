890f17f5d14e79e6550fc42b32f9ff0b815ef07d
Xavier Hanin <xavier.hanin@gmail.com>
2012-04-15 14:53:53 +0200

    make token and dest server parameters in helper scripts + add a script to run them all

--- voxxr-web/src/test/rest/post-events.sh
+++ voxxr-web/src/test/rest/post-events.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-curl   --header "Content-type: application/json" --header "Authorization: Qh12EEHzVPn2AkKfihVs" --request POST  --data @src/test/rest/event.bxjug.json  http://voxxr-web.appspot.com/r/events
+SERVER=$1
+TOKEN=$2
+
+curl   --header "Content-type: application/json" --header "Authorization: ${TOKEN}" --request POST  --data @src/test/rest/event.bxjug.json  ${SERVER}/r/events
