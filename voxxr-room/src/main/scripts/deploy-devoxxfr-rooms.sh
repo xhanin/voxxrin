ec53bb19294fc2dbd21ecd47b55918ad30de32f4
Xavier Hanin <xavier.hanin@gmail.com>
2012-04-17 22:33:31 +0200

    add script to start all devoxx rooms servers

---
+++ voxxr-room/src/main/scripts/deploy-devoxxfr-rooms.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+voxxr-room/src/main/scripts/deploy-room.sh 106394 dvx6-12 voxxr-r1 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106395 dvx6-13 voxxr-r2 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106396 dvx6-14 voxxr-r3 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106397 dvx6-15 voxxr-r4 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106398 dvx6-16 voxxr-r5 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106399 dvx6-17 voxxr-r6 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106400 dvx6-18 voxxr-r7 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106401 dvx6-19 voxxr-r8 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106402 dvx6-20 voxxr-r9 64m 64m
+voxxr-room/src/main/scripts/deploy-room.sh 106403 dvx6-21 voxxr-r10 64m 64m
