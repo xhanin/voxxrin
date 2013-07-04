926918ed48d177254b3121f5287d4fde12fdca43
fcamblor <frederic.camblor@4sh.fr>
2013-07-04 09:56:14 +0200

    added sitemap.xml allowing to provide entry indexation point on /index#!events

---
+++ voxxr-web/src/main/java/voxxr/web/SitemapServlet.java
@@ -0,0 +1,21 @@
+package voxxr.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author fcamblor
+ */
+public class SitemapServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/xml");
+        getServletContext().getRequestDispatcher("/WEB-INF/sitemap.jsp").forward(request, response);
+    }
+}
