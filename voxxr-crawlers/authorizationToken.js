1ec9a7b5298616b35edd1e94617e72dd6dcfaf3b
fcamblor <frederic.camblor@4sh.fr>
2014-05-16 23:45:37 +0200

    provided simpler authorization tokens for dev

--- voxxr-crawlers/authorizationToken.js
+++ voxxr-crawlers/authorizationToken.js
@@ -1,12 +1,12 @@
 module.exports = {
     gae: 'Qh12EEHzVPn2AkKfihVs',
     crawlersFamilies : {
-        1: 'h4FX5x2cAMDZsqxr52Ws',
-        2: '76I7R4D6eH784sjwd2og',
-        3: 'E1266V8A6d112PdZ8n8y',
-        4: 'E2LA764eh1I85gWle82H',
-        5: 'OTq4wfvzS5mBiSK8pE5k',
-        6: 'X1l0PGC01nr8G831L7mx',
-        'all': 'wCy112pZ1aH61q6Io1q4'
+        1: 'devoxxFrToken',
+        2: 'bzhToken',
+        3: '8ac0a4Pp60qU85C4g670',
+        4: 'jugSummercampToken',
+        5: 'leanKanbanToken',
+        6: 'mixitToken',
+        'all': 'adminToken'
     }
 };
